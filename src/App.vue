a8b9c1b75b11f93f3c6a9d02f213ccb74ee75fc1
Omar Flores <omarenriquefq@gmail.com>
2019-07-14 15:51:57 -0400

    Added file structure and components

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,26 @@
 <template>
   <v-app dark>
-    <v-toolbar dark>
-      <v-toolbar-title>Omar Flores</v-toolbar-title>
-    </v-toolbar>
     <v-content>
-      <app-circular-menu></app-circular-menu>
+      <app-home></app-home>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import CircularMenu from "./components/CircularMenu.vue";
+import Home from "./components/Home.vue";
 
 export default {
   name: "App",
   components: {
-    appCircularMenu: CircularMenu
+    appHome: Home
   },
-  data() {
-    return {
-      menuItems: [
-        { icon: "description", title: "Resumé", link: "/" },
-        { icon: "settings_ethernet", title: "Projects", link: "/" }
-      ]
-    };
-  }
+  data() {}
 };
 </script>
 
 <style>
-
 html {
   font-size: 62.5%;
 }
-body {
-  min-height: 100vh;
-  margin: 0;
-  padding: 0;
-}
-
 
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container grid-list-md text-xs-center>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <h3 class="display-3 mb-5">Hello there!</h3>
+                <br>
+                <v-btn color="success">GitHub</v-btn>
+                <v-btn color="error">Projects</v-btn>
+                <v-btn color="warning">Contact</v-btn>
+                <v-btn color="info">Resume</v-btn>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<style scoped>
+  
+</style>
