f0a366613e192a9960f954a70ecec6a46255d333
Omar Flores <omarenriquefq@gmail.com>
2019-07-24 11:44:34 -0400

    Separated backend and frontend for deployment

--- server/index.js
+++ server/index.js
@@ -10,18 +10,6 @@ const app =  express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// Handle Production
-if (process.env.NODE_ENV === 'production') {
-    // Static folder
-    app.use(express.static(__dirname + '/public'));
-
-    // Handle SPA
-    app.get(/.*/, (req, res, next) => {
-        res.sendFile(__dirname + '/public/index.html');
-    });
-}
-const port = process.env.PORT || 5000;
-
 // Email Sending Setup
 const transporter = nodemailer.createTransport(sendgridTransport({
     auth: {
@@ -50,6 +38,8 @@ app.post('/', (req, res, next) => {
         }
     });
 });
+
+const port = process.env.PORT || 5000;
 
 // Port Listen
 app.listen(port, () => {
