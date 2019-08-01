8fc820a01185c517d6a0b9d5a76d8ac3ce941613
Omar Flores <omarenriquefq@gmail.com>
2019-08-01 14:57:41 -0400

    Updated SendGrid API Key and added important headers

--- server/index.js
+++ server/index.js
@@ -18,6 +18,19 @@ const transporter = nodemailer.createTransport(sendgridTransport({
     }
 }));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
 app.post('/', (req, res, next) => {
     const name = req.body.name;
     const email = req.body.email;
